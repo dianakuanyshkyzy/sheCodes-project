6c41bafd22c21074b4118087a8e5be1be4d4e945
Diana Kuanyshkyzy <diana@MacBook-Air-Diana.local>
2024-09-14 20:24:29 +0500

    add: landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import VolunteerSection from "./pages/main";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <VolunteerSection />
+    <Header />
   );
 }
